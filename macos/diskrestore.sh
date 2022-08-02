faf9f0bb12d4ed765aea91f7e9df0d2d4276a419
Antoni Sawicki <tenox@google.com>
2022-08-01 19:04:23 -0700

    add offset

--- macos/diskrestore.sh
+++ macos/diskrestore.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 # disk image restore for macos
-usg="usage: $0 [-z] <file> <disk#>"
+usg="usage: $0 [-z] <file> <disk#> [offset]"
 [ "${1}" = "-z" ] && { erase=true; shift; }
 src="${1?:No src file specified, ${usg}}"
 dst="/dev/rdisk${2?:No dst disk# specified, ${usg}}"
+ofs="${3:-0}"
 bs="1m"
 diskutil unmountDisk "${dst}"
 [ "${erase}" = true ] && diskutil zeroDisk short "${dst}"
@@ -14,5 +15,5 @@ case "${src}" in
     *.bz2) cmd="bzip2 -dc ${src}";;
     *)     cmd="dd if=${src} bs=${bs} status=none";;
 esac
-${cmd} | sudo dd of="${dst}" bs="$bs" status="progress"
+${cmd} | sudo dd of="${dst}" bs="${bs}" status="progress" oseek="${ofs}"
 diskutil eject "${dst}"
