3d8abb8535ec247d906439f31314d2f27a8e4e05
Antoni Sawicki <tenox@google.com>
2022-08-01 19:10:58 -0700

    support spaces in file names

--- macos/diskrestore.sh
+++ macos/diskrestore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-# disk image restore for macos
+# disk image restore hack for macos
 usg="usage: $0 [-z] <file> <disk#> [offset]"
 [ "${1}" = "-z" ] && { erase=true; shift; }
 src="${1?:No src file specified, ${usg}}"
@@ -9,11 +9,11 @@ bs="1m"
 diskutil unmountDisk "${dst}"
 [ "${erase}" = true ] && diskutil zeroDisk short "${dst}"
 case "${src}" in
-    *.lz)  cmd="lzip -dc ${src}";;
-    *.xz)  cmd="xz -dc ${src}";;
-    *.gz)  cmd="gzip -dc ${src}";;
-    *.bz2) cmd="bzip2 -dc ${src}";;
-    *)     cmd="dd if=${src} bs=${bs} status=none";;
+    *.lz)  cmd="lzip -dc '${src}'";;
+    *.xz)  cmd="xz -dc '${src}'";;
+    *.gz)  cmd="gzip -dc '${src}'";;
+    *.bz2) cmd="bzip2 -dc '${src}'";;
+    *)     cmd="dd if='${src}' bs=${bs} status=none";;
 esac
-${cmd} | sudo dd of="${dst}" bs="${bs}" status="progress" oseek="${ofs}"
+eval "${cmd}" | sudo dd of="${dst}" bs="${bs}" status="progress" oseek="${ofs}"
 diskutil eject "${dst}"
